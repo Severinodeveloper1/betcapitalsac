42b8f50e0e6a9adc9b298819857a9ce2c845aeb8
Severinodeveloper1 <developer1@vesergenperu.com>
2026-07-31 11:20:38 -0500

    update

--- app/Filament/Resources/DriverApplications/Pages/ListDriverApplications.php
+++ app/Filament/Resources/DriverApplications/Pages/ListDriverApplications.php
@@ -3,17 +3,9 @@
 namespace App\Filament\Resources\DriverApplications\Pages;
 
 use App\Filament\Resources\DriverApplications\DriverApplicationResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListDriverApplications extends ListRecords
 {
     protected static string $resource = DriverApplicationResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [
-            CreateAction::make(),
-        ];
-    }
 }
